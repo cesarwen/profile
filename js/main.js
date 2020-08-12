2fa3400c0f50cbcfde116f0fc00909b20e7c8dc0
Cesar Wen <csr.cwen@gmail.com>
2020-08-11 22:58:38 -0300

    javascript update, new json for tests

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
-var nome = document.querySelector(".box");
-var person = nome.querySelector("h1");
-person.textContent = "Cesar Wen";
-person.style.color = "#123456"
-person.classList.add("uhuul")
-console.log(nome);
-
-
-var headers = document.querySelectorAll("h2");
-console.log(headers);
-
-for (var i = 0; i < headers.length; i++) {
-    console.log(i);
-    if (i == 2) {
-        console.log("Hello!")
-    }
-}
-
 var xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://raw.githubusercontent.com/cesarwen/profile/master/resourses/test.json");
 
 xhr.addEventListener("load", function() {
-
+    
     var resposta = xhr.responseText;
     var posts = JSON.parse(resposta);
     posts.forEach(function(post) {
-        console.log(post.title);
-        
+        var post_section = document.querySelector("#up_to");
+        var temp = buildPost(post);
+        console.log(posts)
+        console.log(temp)
+        post_section.appendChild(temp);
     });
 });
 
 xhr.send();
+
+function buildPost(post){
+    
+    var post_block = document.createElement("div");
+    post_block.classList.add("post");
+    
+    post_block.appendChild(buildImage(post.nome_imagem));
+    post_block.appendChild(buildBody(post.title, post.content, post.endnote));
+    
+
+    return(post_block)
+}
+
+function buildImage(resource){
+    var path = "resourses/";
+    var div = document.createElement("div");
+    div.classList.add("thumbnail");
+
+    var img = document.createElement("img");
+    img.id  = "img";
+    img.src = path+resource;
+
+    div.appendChild(img);
+
+    return(div)
+}
+
+function buildBody(title, content, note){
+    var path = "resourses/";
+    var div = document.createElement("div");
+    div.classList.add("thumbnail");
+
+    var img = document.createElement("img");
+    img.id  = "img";
+    img.src = path+resource;
+
+    div.appendChild(img);
+
+    return(div)
+}
